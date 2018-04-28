406086f628a1ce10f8030e83044d433982b8a500
patdaburu <pat@daburu.net>
2018-04-28 13:41:40 -0500

    Added modules.

--- setup.py
+++ setup.py
@@ -39,6 +39,10 @@ setup(
     # Here are a couple of examples...
     # 'numpy>=1.13.3,<2',
     # 'measurement>=1.8.0,<2'
+    'psycopg2-binary>=2.7.4,<3',
+    'GeoAlchemy2>=0.4.2,<1',
+    'SQLAlchemy>=1.2.6,<2',
+    'titlecase>=0.12.0,<1'
   ],
   python_requires=">=3.6",
   license='MIT',
